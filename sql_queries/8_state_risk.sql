9027675ea0e0d046f5a05c8076e5d8571b585ba9
semaybulut <semaybulut@gmail.com>
2025-12-08 18:05:55 +0300

    Estimated_Financial_Loss added to 8_state_risk.sql

--- sql_queries/8_state_risk.sql
+++ sql_queries/8_state_risk.sql
@@ -12,9 +12,14 @@ SELECT
   ROUND(COUNTIF(Is_Cancelled = 1) / COUNT(*) * 100, 2) as State_Risk_Score,
   
   -- KPI: Ortalama Rötar
-  ROUND(AVG(IF(Is_Cancelled=0, Dep_Delay, NULL)), 2) as Avg_Delay
+  ROUND(AVG(IF(Is_Cancelled=0, Dep_Delay, NULL)), 2) as Avg_Delay,
+
+  -- YENİ KPI: Tahmini Finansal Kayıp ($)
+  -- Varsayım: Her iptal ortalama 5.000$ maliyet yaratır (Tazminat + Ekip + Otel)
+  (COUNTIF(Is_Cancelled = 1) * 5000) as Estimated_Financial_Loss
 
 FROM `aviation-480106.aviation_ds2.master_table`
 WHERE Dep_State IS NOT NULL
 GROUP BY 1
-ORDER BY State_Risk_Score DESC;
+-- Sıralamayı artık 'Paraya' göre yapalım ki en çok zarar eden eyalet en üste gelsin
+ORDER BY Estimated_Financial_Loss DESC;
